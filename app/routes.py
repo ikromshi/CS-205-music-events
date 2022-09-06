bf06457557e64db65c99553d5a91aff0841b56f8
Ikrom Numonov <numanovikrom454@gmail.com>
2022-09-06 01:38:43 -0400

    lab 2 complete

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,22 @@
 from flask import render_template
 from app import app
 
-@app.route("/")
+
+@app.route('/')
+@app.route('/index')
 def index():
-  return "Ikromshi"
+  return render_template('index.html')
+
+
+@app.route('/artists')
+def artists():
+  artists = ["John Brown's Body", "Gunpoets", "Donna The Buffalo", "The Blind Spots"]
+  return render_template('artists.html',  artists=artists)
+
+@app.route('/artist')
+def artist():
+  return render_template('artist.html')
+
+@app.route('/new_artist')
+def new_artist():
+  return render_template('new_artist.html')
