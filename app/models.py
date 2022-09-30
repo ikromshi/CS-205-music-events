4bd0e50d82fa63866f7612595a19216a7ab3a869
Ikrom Numonov <numanovikrom454@gmail.com>
2022-09-29 22:08:57 -0400

    routes working; events/venues not dislpaying

--- app/models.py
+++ app/models.py
@@ -4,26 +4,30 @@ from datetime import datetime
 
 class Artist(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(64), index=True)
-    last_name = db.Column(db.String(64), index=True)
+    name = db.Column(db.String(64), index=True)
     about_me = db.Column(db.String(140))
-    song_to_art = db.relationship("SongToArtist", backref="artist", lazy="dynamic")
     events = db.relationship("ArtistToEvent", backref="artists", lazy="dynamic")
 
     def __repr__(self):
-        return "<User {}>".format(self.first_name)
+        return "<User {}>".format(self.name)
+
+    def set_artist(self, name, about_me_in):
+        self.name = name
+        self.about_me = about_me_in
 
 
 class Venue(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     location = db.Column(db.String(64), index=True)
     size = db.Column(db.Integer, index=True)
-    events = db.relationship("Event", backref="location", lazy="dynamic")
+    events = db.relationship("Event", backref="venue", lazy="dynamic")
 
 
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.DateTime, default=datetime.utcnow)
+    title = db.Column(db.String(64), index=True)
+    location = db.Column(db.String(64), index=True)
+    date_time = db.Column(db.DateTime, index=True)
     venue_id = db.Column(db.Integer, db.ForeignKey("venue.id"))
     artists = db.relationship("ArtistToEvent", backref="events", lazy="dynamic")
 
@@ -32,15 +36,3 @@ class ArtistToEvent(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     artist_id = db.Column(db.Integer, db.ForeignKey("artist.id"))
     event_id = db.Column(db.Integer, db.ForeignKey("event.id"))
-
-
-class Song(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, index=True)
-    song_to_art = db.relationship("SongToArtist", backref="song", lazy="dynamic")
-
-
-class SongToArtist(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    artist_id = db.Column(db.Integer, db.ForeignKey("artist.id"))
-    song_id = db.Column(db.Integer, db.ForeignKey("song.id"))
--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,34 @@
+from datetime import datetime
 from flask import render_template, flash, redirect, url_for
 from app import app, db
 from app.forms import NewArtist
-from app.models import Artist, Venue, Event
+from app.models import Artist, ArtistToEvent, Venue, Event
 
 
 @app.route("/populate_db")
 def populate_db():
-  u1 = Artist(first_name="Doug", last_name="Turnbull", about_me="Professor")
-  u2 = Artist(first_name="Toby", last_name="Dragon", about_me="Professor")
-  u3 = Artist(first_name="John", last_name="Barr", about_me="Professor")
-  db.session.add_all([u1, u2, u3])
+  reset_db()
+
+  a1 = Artist(name="Denny", about_me="Musician")
+  a2 = Artist(name="Donavan", about_me="Musician")
+  a3 = Artist(name="Tyler", about_me="Musician")
+  a4 = Artist(name="Drew", about_me="Musician")
   db.session.commit()
+  db.session.add_all([a1, a2, a3, a4])
+
+  v1 = Venue(location="Ithaca", size=120)
+  v2 = Venue(location="Ithaca", size=200)
+  v3 = Venue(location="Cambridge", size=750)
+  v4 = Venue(location="NYC", size=150000)
+  db.session.commit()
+  db.session.add_all([v1, v2, v3, v4])
+
+  e1 = Event(title="Live Music Jam", location="Ithaca Commons", date_time=datetime.datetime(2022, 7, 23, 17, 0, 0),venue_id=1)
+  e2 = Event(title="Hip Hop Night", location="local station",date_time=datetime.datetime(2024, 1, 13, 7, 30, 0),venue_id=1)
+  e3 = Event(title="Porchfest Again", location="Cambridge Musical Ceremony", date_time=datetime.datetime(2023, 12, 30, 19, 0, 0),venue_id=3)
+  e4 = Event(title="Birthday Celebration", location="drive way", date_time=datetime.datetime(2024, 1, 1, 12, 0, 0), venue_id=2)
+  db.session.commit()
+  db.session.add_all([e1, e2, e3, e4])
 
 
 @app.route('/')
@@ -21,13 +39,16 @@ def index():
 
 @app.route('/artists')
 def artists():
-  artists = ["John Brown's Body", "Gunpoets", "Donna The Buffalo", "The Blind Spots"]
+  artists = db.session.query(Artist).all()
   return render_template('artists.html',  artists=artists)
 
 
-@app.route('/artist')
-def artist():
-  return render_template('artist.html')
+@app.route('/artist/<name>')
+def artist(name):
+  artist = db.session.query(Artist).filter_by(name=name).first()
+  events = db.session.query(Event).join(ArtistToEvent, ArtistToEvent.event_id == Event.id).join(Artist, Artist.id == ArtistToEvent.artist_id).filter(Artist.name == name).all()
+
+  return render_template('artist_page.html', title=name, artist=artist, events=events)
 
 
 @app.route('/new_artist', methods=['GET', 'POST'])
@@ -38,3 +59,16 @@ def new_artist():
     #   form.name.data, form.hometown.data))
     return render_template("new_artist.html", form=form, name=form.name.data, hometown=form.hometown.data, description=form.description.data)
   return render_template('new_artist.html', form=form)
+
+
+def reset_db():
+   flash("Resetting database: deleting old data and repopulating with dummy data")
+   # clear all data from all tables
+   meta = db.metadata
+   for table in reversed(meta.sorted_tables):
+       print('Clear table {}'.format(table))
+       db.session.execute(table.delete())
+   db.session.commit()
+
+  # now create Artist, Venues, Events, and ArtistToEvent Objects and persist them to the db
+
